879d49342c90418db368c8caf51be2299de16fcd
faizan-shaikh-dev <faizangulab4276@gmail.com>
2026-06-18 22:39:15 +0530

    ..

--- chat-backend/routes/messageRoutes.js
+++ chat-backend/routes/messageRoutes.js
@@ -17,6 +17,6 @@ router.get("/getAll/:userId", protect, getMessages);
 router.patch("/seen/:messageId", protect, markAsSeen);
 router.post("/upload-image", protect, upload.single("image"), uploadImage);
 router.post("/upload-video", protect, upload.single("video"), uploadVideo);
-router.post("/send-image/:receiverId", protect, upload.single("image", sendImageMessage))
+router.post("/send-image/:receiverId", protect, upload.single("image", sendImageMessage));
 
 export default router;
