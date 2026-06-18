c7638c14956debdb47e97d51baa70c29b87cfe64
faizan-shaikh-dev <faizangulab4276@gmail.com>
2026-06-18 19:07:49 +0530

    chat Api and socket steup completed

--- chat-backend/controllers/messageController.js
+++ chat-backend/controllers/messageController.js
@@ -1,48 +1,82 @@
 import Message from "../models/messageModel.js";
+import { getIO, onlineUsers } from "../socket/socket.js";
 
 //Send Message Logic
-export const sendMessage = async (req, res) =>{
-    try {
-        const {receiverId} = req.params;
-        const {text} = req.body;
+export const sendMessage = async (req, res) => {
+  try {
+    const { receiverId } = req.params;
+    const { text } = req.body;
 
-        const newMessage = await Message.create({
-            senderId: req.user.userId,
-            receiverId, 
-            text,
-            messageType: "text",
-        });
+    const newMessage = await Message.create({
+      senderId: req.user.userId,
+      receiverId,
+      text,
+      messageType: "text",
+    });
 
-        return res.status(200).json({message:"message sent", newMessage});
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({message:"Internal Server Error"})
-        
+    const receiverSockedId = onlineUsers.get(receiverId);
+
+    if (receiverSockedId) {
+      getIO().to(receiverSockedId).emit("receive-message", newMessage);
     }
-};
 
+    return res.status(200).json({ message: "message sent", newMessage });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 //Get Message Logic
 
-export const getMessages = async (req, res)=>{
-    try {
-        const otherUserId = req.params.userId;
+export const getMessages = async (req, res) => {
+  try {
+    const otherUserId = req.params.userId;
 
-        const messages = await Message.find({$or: [{
-            senderId: req.user.userId,
-            receiverId:otherUserId
+    const messages = await Message.find({
+      $or: [
+        {
+          senderId: req.user.userId,
+          receiverId: otherUserId,
         },
 
         {
-            senderId: otherUserId,
-            receiverId:req.user.userId
-        }
-    ]
-    }).sort({createdAt: 1});
-    return res.status(200).json({messages});
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({message:"Internal Server Error"});
-        
-    }
+          senderId: otherUserId,
+          receiverId: req.user.userId,
+        },
+      ],
+    }).sort({ createdAt: 1 });
+    return res.status(200).json({ messages });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+//marka Seen Logic
+
+export const markAsSeen = async (req, res) => {
+  try {
+    const { messageId } = req.params;
+
+    const message = await Message.findByIdAndUpdate(
+      messageId,
+      {
+        isRead: true,
+      },
+      {
+        new: true,
+      },
+    );
+
+    return res.status(200).json({
+      message: "Message marked as seen",
+      data: message,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 };
--- chat-backend/routes/messageRoutes.js
+++ chat-backend/routes/messageRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { getMessages, sendMessage } from "../controllers/messageController.js";
+import { getMessages, markAsSeen, sendMessage } from "../controllers/messageController.js";
 
 const router = express.Router();
 
 router.post("/sendMessage/:receiverId", protect, sendMessage);
 router.get("/getAll/:userId",protect,getMessages);
+router.patch("/seen/:messageId",protect, markAsSeen)
 
 export default router;
