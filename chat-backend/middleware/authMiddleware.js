08dd9caa0ab66438cb733ee97131301cbbc1b5ac
faizan-shaikh-dev <faizangulab4276@gmail.com>
2026-06-18 16:54:01 +0530

    generate middleware and fix authRoutes bugs

---
+++ chat-backend/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+ export const protect = (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        message: "no token provided",
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({
+      message: "Invalid Token",
+    });
+  }
+};
